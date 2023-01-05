e116e917ed20156ec0d239f607f500e6708c1541
brince0304 <brince0304@gmail.com>
2023-01-05 20:32:01 +0900

    #6 - 데이터 파싱용 유틸 클래스 메소드 구현

--- src/main/java/com/dfoff/demo/Util/OpenAPIUtil.java
+++ src/main/java/com/dfoff/demo/Util/OpenAPIUtil.java
@@ -22,39 +22,67 @@ public class OpenAPIUtil {
     public static final String API_KEY = "qQpswERaNSg1ifEA7rbze6oNJrej4JJW";
     private static final Gson gson = getGson();
 
+    public static final String LIMIT = "50";
+
+    public static final String WORD_TYPE = "full";
+
+    public static final String SERVER_LIST_URL = "https://api.neople.co.kr/df/servers?apikey=" + API_KEY;
+
+    public static final String JOB_LIST_URL = "https://api.neople.co.kr/df/jobs?apikey=" + API_KEY;
+
+    public static final String CHARACTER_IMG_URL = "https://img-api.neople.co.kr/df/servers/<serverId>/characters/<characterId>?zoom=<zoom>";
+
+    public static final String CHARACTER_SEARCH_URL = "https://api.neople.co.kr/df/servers/<serverId>/characters?characterName=<characterName>&jobId=<jobId>&jobGrowId=<jobGrowId>&limit="+LIMIT+"&wordType="+WORD_TYPE+"&apikey="+API_KEY;
+
+
+
 
     @Bean
     public static Gson getGson() {
         return new Gson();
     }
 
+
+    public static String getCharacterSearchUrl(String serverId, String characterName) {
+        return CHARACTER_SEARCH_URL.replace("<serverId>", serverId)
+                .replace("<characterName>", characterName).replace("<jobId>", "").replace("<jobGrowId>", "");
+    }
+    public static String getCharacterImgUrl(String serverId, String characterId, String zoom) {
+        return CHARACTER_IMG_URL.replace("<serverId>", serverId)
+                .replace("<characterId>", characterId)
+                .replace("<zoom>", zoom);
+    }
     public static <T> T parseUtil(String url, Class<T> clazz) {
         HashMap<String, String> result = new HashMap<>();
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders header = new HttpHeaders();
         HttpEntity<?> entity = new HttpEntity<>(header);
-        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url.replace("<apikey>", API_KEY)).build();
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
         ResponseEntity<?> response = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, String.class);
+        log.info("response: {}", response);
+        log.info("url: {}", url);
         return gson.fromJson(response.getBody().toString(), clazz);
     }
 
-    public static HashMap<List<DFJob>, List<DFJobGrow>> parseDFJobDTO(DFJob.DFJobDTO dfJobDTO) {
+    public static HashMap<List<DFJob>, List<DFJobGrow>> parseDFJobDTO(DFJob.DFJobJSONDTO dfJobJSONDTO) {
         HashMap<List<DFJob>, List<DFJobGrow>> result = new HashMap<>();
         List<DFJob> dfJobList = new ArrayList<>();
         List<DFJobGrow> dfJobGrowList = new ArrayList<>();
-        for (DFJob.DFJobDTO.Row row : dfJobDTO.getRows()) {
+        for (DFJob.DFJobJSONDTO.Row row : dfJobJSONDTO.getRows()) {
             DFJob dfJob = DFJob.builder().jobId(row.getJobId()).jobName(row.getJobName()).build();
+            DFJobGrow dfJobGrow = DFJobGrow.builder().jobGrowId(row.getJobId()).jobGrowName(row.getJobName()).jobName(dfJob).build();
             dfJobList.add(dfJob);
-            for (DFJob.DFJobDTO.Row__1 row__1 : row.getRows()) {
+            dfJobGrowList.add(dfJobGrow);
+            for (DFJob.DFJobJSONDTO.Row__1 row__1 : row.getRows()) {
                 dfJobGrowList.add(DFJobGrow.builder().jobName(dfJob).jobGrowId(row__1.getJobGrowId()).jobGrowName(row__1.getJobGrowName()).build());
                 if (row__1.getNext() != null) {
-                    DFJob.DFJobDTO.Next next = row__1.getNext();
+                    DFJob.DFJobJSONDTO.Next next = row__1.getNext();
                     dfJobGrowList.add(DFJobGrow.builder().jobName(dfJob).jobGrowId(next.getJobGrowId()).jobGrowName(next.getJobGrowName()).build());
                     if (next.getNext() != null) {
-                        DFJob.DFJobDTO.Next__1 next1 = next.getNext();
+                        DFJob.DFJobJSONDTO.Next__1 next1 = next.getNext();
                         dfJobGrowList.add(DFJobGrow.builder().jobName(dfJob).jobGrowId(next1.getJobGrowId()).jobGrowName(next1.getJobGrowName()).build());
                         if (next1.getNext() != null) {
-                            DFJob.DFJobDTO.Next__2 next2 = next1.getNext();
+                            DFJob.DFJobJSONDTO.Next__2 next2 = next1.getNext();
                             dfJobGrowList.add(DFJobGrow.builder().jobName(dfJob).jobGrowId(next2.getJobGrowId()).jobGrowName(next2.getJobGrowName()).build());
                         }
                     }
