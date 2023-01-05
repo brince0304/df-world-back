38e3ebd6cf3285dfb7413ad0a39d048c5b5b2802
brince0304 <brince0304@gmail.com>
2023-01-05 23:31:59 +0900

    #6 - 파일 저장용 엔티티 설계 및 핵심 로직 구현

---
+++ src/main/java/com/dfoff/demo/Util/FileUtil.java
@@ -0,0 +1,54 @@
+package com.dfoff.demo.Util;
+
+import com.dfoff.demo.Domain.SaveFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+public class FileUtil {
+    private FileUtil() {
+    }
+    @Value("${com.example.upload.path.profileImg}")
+    public static String uploadPath;
+
+
+    public  static String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+    public static String getFileNameWithUUID(String fileName) {
+        return UUID.randomUUID().toString() + "_" + fileName;
+    }
+
+    public static File getMultipartFileToFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(uploadPath,getFileNameWithUUID(multipartFile.getOriginalFilename()));
+        multipartFile.transferTo(file);
+        return file;
+    }
+
+    public static File getFileFromSaveFile(SaveFile.SaveFileDTO saveFileDto) {
+        return new File(saveFileDto.getFilePath()+"/"+saveFileDto.getFileName());
+    }
+
+
+    public static void deleteFile(SaveFile.SaveFileDTO profileImg) {
+        File file = getFileFromSaveFile(profileImg);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+    public static SaveFile.SaveFileDTO getFileDtoFromMultiPartFile(MultipartFile multipartFile) throws IOException {
+        String fileName = getMultipartFileToFile(multipartFile).getName();
+        String fileType = getExtension(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        Long fileSize = multipartFile.getSize();
+        return SaveFile.SaveFileDTO.builder()
+                .fileName(fileName)
+                .filePath("/Users/brinc/Desktop/brincestudy/JAVA/df-toy-project/src/main/resources/static/images/imgSaveFolder"+fileName)
+                .fileType(fileType)
+                .fileSize(fileSize)
+                .build();// TODO: 경로가 자꾸 null 로 입력되기 때문에 해결 방안을 찾아야함.
+    }
+}
