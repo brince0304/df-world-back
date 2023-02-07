de43d2e48a460f9e6955ebeea6406a90c83f5d73
brince0304 <brince0304@gmail.com>
2023-02-07 21:20:56 +0900

    #22 - Param 어노테이션 추가

--- src/main/java/com/dfoff/demo/Repository/BoardHashtagMapperRepository.java
+++ src/main/java/com/dfoff/demo/Repository/BoardHashtagMapperRepository.java
@@ -2,13 +2,16 @@ package com.dfoff.demo.Repository;
 
 import com.dfoff.demo.Domain.Board;
 import com.dfoff.demo.Domain.BoardHashtagMapper;
+import com.dfoff.demo.Domain.EnumType.BoardType;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardHashtagMapperRepository extends org.springframework.data.jpa.repository.JpaRepository<com.dfoff.demo.Domain.BoardHashtagMapper, Long> {
    @Query("select bhm from BoardHashtagMapper bhm where bhm.hashtag.name = :name and bhm.board.deleted=false")
-    Page<BoardHashtagMapper> findAllByHashtagName(String name, org.springframework.data.domain.Pageable pageable);
+    Page<BoardHashtagMapper> findAllByHashtagName(@Param("name") String name, Pageable pageable);
 
     @Query("select bhm from BoardHashtagMapper bhm where bhm.hashtag.name=:name and bhm.board.deleted=false and bhm.board.boardType=:boardType")
-    Page<BoardHashtagMapper> findAllByHashtagNameAndBoardType(String name, com.dfoff.demo.Domain.EnumType.BoardType boardType, org.springframework.data.domain.Pageable pageable);
+    Page<BoardHashtagMapper> findAllByHashtagNameAndBoardType(@Param("name") String name,@Param("boardType") BoardType boardType, Pageable pageable);
 }
