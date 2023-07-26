147c43e385c2b0bdc8106464be3f452fbe993ef6
brince0304 <brince0304@gmail.com>
2023-07-27 02:03:25 +0900

    fix: 스크립트 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO SAVE_FILE (FILE_NAME, FILE_PATH, FILE_SIZE, FILE_TYPE, CREATED_AT, CREATED_BY, MODIFIED_AT, MODIFIED_BY)
+INSERT INTO save_file (file_name, file_path, file_size, file_type, created_at, created_by, modified_at, modified_by)
 VALUES ('icon_char_0.png', '/home/ubuntu/df-project/df-toy-project/src/main/resources/static/images/icon_char/icon_char_0.png', '0', '.png', NOW(), 'brinc', NOW(), 'brinc');
 
-INSERT INTO USER_ACCOUNT (USER_ID, CREATED_AT, CREATED_BY, MODIFIED_AT, MODIFIED_BY, EMAIL, NICKNAME, PASSWORD, PROFILE_ICON)
+INSERT INTO user_account (user_id, created_at, created_by, modified_at, modified_by, email, nickname, password, profile_icon)
 VALUES ('test', NOW(), 'test', NOW(), 'test', 'test@email.com', '안녕내이름은방가', '$2a$10$DAd.J6N1fv8ecD0UsYKOu.yPnrAQe.lw4pJmLaX6d3fhJ5Bzllw5.', 1);
-INSERT INTO USER_ACCOUNT_ROLES (USER_ACCOUNT_USER_ID, ROLES) VALUES ('test', 'ROLE_USER');
+INSERT INTO user_account_roles (user_account_user_id, roles) VALUES ('test', 'ROLE_USER');
