d9bbc897b625ee0895212dc99547e61fd0068599
brince0304 <brince0304@gmail.com>
2023-07-26 22:25:54 +0900

    fix: entityScan 추가

--- src/main/java/com/dfoff/demo/DfOffProjectApplication.java
+++ src/main/java/com/dfoff/demo/DfOffProjectApplication.java
@@ -3,10 +3,12 @@ package com.dfoff.demo;
 import org.apache.logging.log4j.util.ProcessIdUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EntityScan(basePackages = {"com.dfoff.demo.domain"})
 public class DfOffProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DfOffProjectApplication.class, args);
