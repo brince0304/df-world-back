1311f6c96c51426353af791238bbbef856dacc31
brince0304 <brince0304@gmail.com>
2023-07-27 00:54:52 +0900

    fix: 수정

--- src/main/java/com/dfoff/demo/DfOffProjectApplication.java
+++ src/main/java/com/dfoff/demo/DfOffProjectApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EntityScan(basePackages = {"com.dfoff.demo.domain"})
 public class DfOffProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DfOffProjectApplication.class, args);
