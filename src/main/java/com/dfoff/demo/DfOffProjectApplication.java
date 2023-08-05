964383ee72f2cac4c45674811cd7d06eeaa098d3
brince0304 <brince0304@gmail.com>
2023-08-05 13:12:32 +0900

    fix : ssl설정 추가

--- src/main/java/com/dfoff/demo/DfOffProjectApplication.java
+++ src/main/java/com/dfoff/demo/DfOffProjectApplication.java
@@ -3,9 +3,6 @@ package com.dfoff.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -13,18 +10,5 @@ public class DfOffProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DfOffProjectApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedMethods("*")
-				.allowedOrigins("*")
-				.allowedHeaders("*")
-				.allowCredentials(true)
-				.maxAge(3600);
-			}
-		};
-	}
+
 }
